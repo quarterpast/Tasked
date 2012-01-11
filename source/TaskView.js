3f22cfdb2a021509cb0cff17cb84b24c40c68a41
Matt Brennan <mattyb1000@gmail.com>
2012-01-11 20:19:31 +0000

    day borders

--- source/TaskView.js
+++ source/TaskView.js
@@ -28,58 +28,58 @@ enyo.kind({
 					{kind:"ToolButton",icon:"images/Dark/go-last.png"}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 26},
-					{flex: 1, content: 27},
-					{flex: 1, content: 28},
-					{flex: 1, content: 29},
-					{flex: 1, content: 30},
-					{flex: 1, content: 31},
-					{flex: 1, content: 1}
+					{kind:"CustomButton", className:"day",flex: 1, content: 26},
+					{kind:"CustomButton", className:"day",flex: 1, content: 27},
+					{kind:"CustomButton", className:"day",flex: 1, content: 28},
+					{kind:"CustomButton", className:"day",flex: 1, content: 29},
+					{kind:"CustomButton", className:"day",flex: 1, content: 30},
+					{kind:"CustomButton", className:"day",flex: 1, content: 31},
+					{kind:"CustomButton", className:"day",flex: 1, content: 1}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 2},
-					{flex: 1, content: 3},
-					{flex: 1, content: 4},
-					{flex: 1, content: 5},
-					{flex: 1, content: 6},
-					{flex: 1, content: 7},
-					{flex: 1, content: 8}
+					{kind:"CustomButton", className:"day",flex: 1, content: 2},
+					{kind:"CustomButton", className:"day",flex: 1, content: 3},
+					{kind:"CustomButton", className:"day",flex: 1, content: 4},
+					{kind:"CustomButton", className:"day",flex: 1, content: 5},
+					{kind:"CustomButton", className:"day",flex: 1, content: 6},
+					{kind:"CustomButton", className:"day",flex: 1, content: 7},
+					{kind:"CustomButton", className:"day",flex: 1, content: 8}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 9},
-					{flex: 1, content: 10},
-					{flex: 1, content: 11},
-					{flex: 1, content: 12},
-					{flex: 1, content: 13},
-					{flex: 1, content: 14},
-					{flex: 1, content: 15}
+					{kind:"CustomButton", className:"day",flex: 1, content: 9},
+					{kind:"CustomButton", className:"day",flex: 1, content: 10},
+					{kind:"CustomButton", className:"day",flex: 1, content: 11},
+					{kind:"CustomButton", className:"day",flex: 1, content: 12},
+					{kind:"CustomButton", className:"day",flex: 1, content: 13},
+					{kind:"CustomButton", className:"day",flex: 1, content: 14},
+					{kind:"CustomButton", className:"day",flex: 1, content: 15}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 16},
-					{flex: 1, content: 17},
-					{flex: 1, content: 18},
-					{flex: 1, content: 19},
-					{flex: 1, content: 20},
-					{flex: 1, content: 21},
-					{flex: 1, content: 22}
+					{kind:"CustomButton", className:"day",flex: 1, content: 16},
+					{kind:"CustomButton", className:"day",flex: 1, content: 17},
+					{kind:"CustomButton", className:"day",flex: 1, content: 18},
+					{kind:"CustomButton", className:"day",flex: 1, content: 19},
+					{kind:"CustomButton", className:"day",flex: 1, content: 20},
+					{kind:"CustomButton", className:"day",flex: 1, content: 21},
+					{kind:"CustomButton", className:"day",flex: 1, content: 22}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 23},
-					{flex: 1, content: 24},
-					{flex: 1, content: 25},
-					{flex: 1, content: 26},
-					{flex: 1, content: 27},
-					{flex: 1, content: 28},
-					{flex: 1, content: 29}
+					{kind:"CustomButton", className:"day",flex: 1, content: 23},
+					{kind:"CustomButton", className:"day",flex: 1, content: 24},
+					{kind:"CustomButton", className:"day",flex: 1, content: 25},
+					{kind:"CustomButton", className:"day",flex: 1, content: 26},
+					{kind:"CustomButton", className:"day",flex: 1, content: 27},
+					{kind:"CustomButton", className:"day",flex: 1, content: 28},
+					{kind:"CustomButton", className:"day",flex: 1, content: 29}
 				]},
 				{kind:"HFlexBox",components:[
-					{flex: 1, content: 30},
-					{flex: 1, content: 31},
-					{flex: 1, content: 1},
-					{flex: 1, content: 2},
-					{flex: 1, content: 3},
-					{flex: 1, content: 4},
-					{flex: 1, content: 5}
+					{kind:"CustomButton", className:"day",flex: 1, content: 30},
+					{kind:"CustomButton", className:"day",flex: 1, content: 31},
+					{kind:"CustomButton", className:"day",flex: 1, content: 1},
+					{kind:"CustomButton", className:"day",flex: 1, content: 2},
+					{kind:"CustomButton", className:"day",flex: 1, content: 3},
+					{kind:"CustomButton", className:"day",flex: 1, content: 4},
+					{kind:"CustomButton", className:"day",flex: 1, content: 5}
 				]},
 			]}
 		]},
